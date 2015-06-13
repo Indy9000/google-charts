7af48097d102803c08fa769cf75d742d14a5883f
Indeera Munasinghe <Indeera-msrc@outlook.com>
2015-06-13 14:23:20 +0000

    added timeline chart

---
+++ google-timeline-chart.fsx
@@ -0,0 +1,77 @@
+﻿module GoogleCharts 
+    open System
+
+    let gchart_lib = """<script type="text/javascript" src="https://www.google.com/jsapi"></script>"""
+    let js_bar_chart_injectable = 
+        """
+        <script type="text/javascript">
+            google.load("visualization", "1", {packages:["@@CHART-TYPE-1"]});
+            google.setOnLoadCallback(drawChart);
+    
+            function drawChart() {
+                var dataTable = new google.visualization.DataTable();
+
+                dataTable.addColumn({ type: 'string', id: '@@CHART-DATA-HEADER-ROW-LABEL' });
+                dataTable.addColumn({ type: 'string', id: '@@CHART-DATA-HEADER-BAR-LABEL' });
+                dataTable.addColumn({ type: 'string', role: 'tooltip' });
+                dataTable.addColumn({ type: 'date', id: 'Start' });
+                dataTable.addColumn({ type: 'date', id: 'End' });
+
+                dataTable.addRows([
+                    @@DATA-ITEMS
+                  ]);
+
+                var options = {
+                    width: @@CHART-WIDTH,
+                    height:@@CHART-HEIGHT,
+                  chart: {
+                      title: '@@CHART-TITLE',
+                    subtitle: '@@CHART-SUBTITLE'
+                  }
+                };
+
+                var chart = new google.visualization.@@CHART-TYPE-2(document.getElementById('@@CHART-DIV'));
+                chart.draw(dataTable, options);
+            }
+        </script>
+
+        """
+
+    type Timeline_ChartOptions =  {Title:string ; SubTitle:string ; Width:int ; Height:int}
+    type Timeline_ChartData = 
+        {
+            RowLabel:string;
+            BarLabel:string;//optional
+            ToolTip:string;//optional
+
+            Dataset:(string * string * string * DateTime * DateTime)[] //label[] start-date finish-date
+        }
+
+    let InjectGoogleBarChart (options:Timeline_ChartOptions) (data:Timeline_ChartData) (div_id:string) (html:string) =
+        let data_items =
+            data.Dataset 
+            |> Array.map( fun (s1, s2, s3, start, finish) -> 
+                                    sprintf "['%s','%s','%s', new Date(%s), new Date(%s)]"
+                                        s1 s2 s3
+                                        (start.ToString("yyyy,MM,dd,HH,mm,ss"))
+                                        (finish.ToString("yyyy,MM,dd,HH,mm,ss"))
+                        )
+
+        let js = js_bar_chart_injectable
+                    .Replace("@@CHART-TITLE", options.Title)
+                    .Replace("@@CHART-WIDTH", options.Width.ToString())
+                    .Replace("@@CHART-HEIGHT", options.Height.ToString())
+                    .Replace("@@CHART-SUBTITLE", options.SubTitle)
+                    .Replace("@@CHART-DATA-HEADER-ROW-LABEL", data.RowLabel)
+                    .Replace("@@CHART-DATA-HEADER-BAR-LABEL", data.BarLabel)
+                    .Replace("@@DATA-ITEMS", String.Join(",", data_items))
+                    .Replace("@@CHART-DIV",div_id)
+                    .Replace("@@CHART-TYPE-1", "timeline")
+                    .Replace("@@CHART-TYPE-2", "Timeline")
+
+        let k = html.IndexOf("</head>")
+        let payload = match html.Contains(gchart_lib) with
+                        | true -> js
+                        | false -> gchart_lib + js
+        html.Insert(k, payload)
+
---
+++ timeline-chart-how-to.fsx
@@ -0,0 +1,48 @@
+﻿(**
+This script shows how to insert a Google bar/column/line chart to an html page.
+*)
+
+#load "google-timeline-chart.fsx" 
+open System
+open System.IO
+open GoogleCharts
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let your_data = 
+    {
+        RowLabel = "Room";
+        BarLabel = "Name";//optional
+        ToolTip = "ToolTip";//optional
+        Dataset = //label[] start-date finish-date
+            [|
+                ("Magnolia Room",   "CSS Fundamentals",   "", DateTime.Parse("12:00:00"), DateTime.Parse("14:00:00"));
+                ("Magnolia Room",   "Intro JavaScript",   "", DateTime.Parse("14:30:00"), DateTime.Parse("16:00:00"));
+                ("Magnolia Room",   "Advanced JavaScript","", DateTime.Parse("16:30:00"), DateTime.Parse("19:00:00"));
+                ("Gladiolus Room",  "Intermediate Perl",  "", DateTime.Parse("12:30:00"), DateTime.Parse("14:00:00"));
+                ("Gladiolus Room",  "Advanced Perl",      "", DateTime.Parse("14:30:00"), DateTime.Parse("16:00:00"));
+                ("Gladiolus Room",  "Applied Perl",       "", DateTime.Parse("16:30:00"), DateTime.Parse("18:00:00"));
+                ("Petunia Room",    "Google Charts",      "", DateTime.Parse("12:30:00"), DateTime.Parse("14:00:00"));
+                ("Petunia Room",    "Closure",            "", DateTime.Parse("14:30:00"), DateTime.Parse("16:00:00"));
+                ("Petunia Room",    "App Engine",         "", DateTime.Parse("16:30:00"), DateTime.Parse("18:30:00"));
+            |]
+    }
+
+let your_chart_options = 
+    {
+        Title = "Class Time Table";
+        SubTitle = "Example Google chart injection";
+        Width = 900;
+        Height = 500;
+    }
+
+let your_html_template = File.ReadAllText("index.html")
+
+let new_html = 
+    your_html_template 
+    |> InjectGoogleBarChart your_chart_options your_data "timeline-chart-div-id" 
+
+//open in default local browser
+let tmp = Path.ChangeExtension(Path.GetTempFileName(),".html")
+File.WriteAllText(tmp, new_html)
+System.Diagnostics.Process.Start(tmp)
